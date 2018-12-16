186088afa121554bc642d8f97c7b9fbae7ddf035
Marcelo Pasut <pasutmarcelo@gmail.com>
2018-12-15 23:29:18 -0300

    trabajo en el post para obtener el historial de un share

--- get_historical_share.py
+++ get_historical_share.py
@@ -1,50 +1,52 @@
+import datetime
 import json
 from botocore.vendored import requests
 import logging
 import os
 
-logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
+logger = logging.getLogger('get_historical_share_handler')
+logger.setLevel(logging.INFO)
 
 INVERTIRONLINE_PATH = 'https://api.invertironline.com/'
+INVERTIRONLINE_SHARES_URL = INVERTIRONLINE_PATH + 'api/%s/titulos/%s/cotizacion/seriehistorica/%s/%s/sinajustar'
+INVERTIRONLINE_USERNAME = os.environ['INVERTIRONLINE_USERNAME']
+INVERTIRONLINE_PASSWORD = os.environ['INVERTIRONLINE_PASSWORD']
 
 
 def get_historical_share_handler(event, context):
+    index = event['index']
+    share = event['stock_share']
+    fromDate = event.get('from', '2008-01-01')
+    toDate = event.get('to', datetime.date.today().isoformat())
+    url = INVERTIRONLINE_SHARES_URL % (index, share, fromDate, toDate)
     response = requests.post(INVERTIRONLINE_PATH + 'token',
                              headers={
                                  'content-type': 'application/x-www-form-urlencoded',
-                                 'username': os.environ[INVERTIRONLINE_USERNAME],
-                                 'password': os.environ[INVERTIRONLINE_PASSWORD]
+                                 'username': INVERTIRONLINE_USERNAME,
+                                 'password': INVERTIRONLINE_PASSWORD
                              },
                              data={
-                                 'username': os.environ[INVERTIRONLINE_USERNAME],
-                                 'password': os.environ[INVERTIRONLINE_PASSWORD],
+                                 'username': INVERTIRONLINE_USERNAME,
+                                 'password': INVERTIRONLINE_PASSWORD,
                                  'grant_type': 'password'
                              })
+
     response.raise_for_status()
     if response.status_code == 200:
         body = response.json()
         access_token = body['access_token']
-
         response = requests.get(
-            'https://api.invertironline.com/api/bcba/titulos/cado/cotizacion/seriehistorica/2018-11-01/2018-11-17/sinajustar',
+            url,
             headers={
                 'Accept': 'application/json',
                 'Authorization': 'bearer %s' % access_token
             })
-        logger.debug({
-            'Accept': 'application/json',
-            'Authorization': 'bearer %s' % access_token
-        })
         response.raise_for_status()
-        return {
-            'statusCode': response.status_code,
-            'body': response.json()
-        }
-    return {
-        'statusCode': 200,
-        'body': {
-            'share': event['share'],
-            'env': os.environ['saraza']
-        }
-    }
+        if response.status_code == 200:
+            return {
+                'statusCode': 201,
+                'body': response.json()
+            }
+        return {'statusCode': 500}
+
+    return {'statusCode': 500}

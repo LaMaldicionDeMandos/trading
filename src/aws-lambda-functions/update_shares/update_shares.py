5036cd11dc28c4a78f3b3555ba83f1c484827cce
Marcelo Pasut <pasutmarcelo@gmail.com>
2018-12-19 00:07:26 -0300

    trabajo en obtener el update desde invertir online

--- src/aws-lambda-functions/update_shares/update_shares.py
+++ src/aws-lambda-functions/update_shares/update_shares.py
@@ -1,27 +1,29 @@
 import boto3
 import json
 import logging
+import invertir_online_connection
 
 logger = logging.getLogger('update_shares')
 logger.setLevel(logging.INFO)
 
 BUCKET = 'marceyaida-trading'
 
-
 def update_all(event, context):
+    access_token = invertir_online_connection.connect()
+    logger.info("Access Token: %s" % access_token)
     s3 = boto3.client("s3")
     share_names = get_share_names(s3)
     for share_name in share_names:
-        get_share(s3, share_name)
-
-
+        share = get_share(s3, share_name)
+        #Usar los datos que vienen, en share_name esta bcba/meli.json habria que separarlo la fecha hay que usar por defecto la ultima pero se puede tomar por parametro
+        news = invertir_online_connection.get_historical_share(access_token, "bcba", "meli", "2018-12-18", "2018-12-19")
+        logger.info(str(news))
 def get_share_names(s3):
-    return map(lambda it: it['Key'], s3.list_objects(Bucket=BUCKET)['Contents'])
-
+    return map( lambda it: it['Key'], s3.list_objects(Bucket = BUCKET)['Contents'])
 
 def get_share(s3, share_name):
-    obj = s3.get_object(Bucket=BUCKET, Key=share_name)
+    obj = s3.get_object(Bucket = BUCKET, Key = share_name)
     file_content = obj['Body'].read().decode('utf-8')
     data = json.loads(file_content)
-    # Esta ultima linea usa len para saber la cantidad de registros que tiene
     logger.info("%s -> %d" % (share_name, len(data)))
+    return data

a94bd93fec8b4a88531495678730943ef8ae564e
Marcelo Pasut <pasutmarcelo@gmail.com>
2019-01-10 00:33:09 -0300

    agrego error handler

--- src/aws-lambda-functions/get_shares/get_share_names_by_index.py
+++ src/aws-lambda-functions/get_shares/get_share_names_by_index.py
@@ -7,4 +7,7 @@ logger.setLevel(logging.INFO)
 
 def get_share_names(event, context):
     index = event['index']
-    return s3_service.get_share_names_by_index(index)
+    try:
+        return s3_service.get_share_names_by_index(index)
+    except Exception as e:
+        raise UserWarning("No se ha encontrado el indice %s." % index)
